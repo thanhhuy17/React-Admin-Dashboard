f55a0d41430a78e8775b828dff12152aa5c7a484
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-07-11 20:18:42 +0700

    Fix 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+// import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  // plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
+
 });
