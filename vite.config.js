a0893ee0b31aaeb8de65e35b21a2c545dc9c0c22
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-07-11 23:47:33 +0700

    fix13

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var plugin_react_1 = require("@vitejs/plugin-react");
-var vite_1 = require("vite");
-// import tsconfigPaths from "vite-tsconfig-paths";
-exports.default = (0, vite_1.defineConfig)({
-    // plugins: [tsconfigPaths(), react()],
-    plugins: [(0, plugin_react_1.default)()],
-});
